8bd5ee65d39b3fd9e152c3f88b5b43e1bf5fa204
Timothy Smith <timsmith78@yahoo.com>
2018-08-24 19:59:31 -0500

    Add yahoo stub

--- liri.js
+++ liri.js
@@ -123,6 +123,8 @@ function execLiriCmd(cmd) {
                 }
             })
             break
+	case 'yahoo-this'
+	    return
         case 'do-what-it-says':
             return
         default:
